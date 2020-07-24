cda994525867ea80be9cd4a69b353d03ef5a385c
Lohrainee <lohrainee.s@gmail.com>
2020-07-24 17:09:20 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 
@@ -55,6 +58,8 @@ public class DepartmentFormController implements Initializable {
 	public void subscribeDataChangeListener (DataChangeListener listener) {
 		dataChangeListeners.add(listener);
 	}
+	
+	
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
 		
@@ -69,6 +74,9 @@ public class DepartmentFormController implements Initializable {
 			
 			Utils.currentStage(event).close();
 			
+		}
+		catch (ValidationException e ) {
+			setErrorMessages (e.getErros());
 		}
 		catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
@@ -85,7 +93,20 @@ public class DepartmentFormController implements Initializable {
 	private Department getFormData() {
 		Department obj = new Department();
 		
+		ValidationException exception = new ValidationException ("Validation error");
+
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if (txtName.getText() == null || txtName.getText().trim().equals(" ")) {
+			exception.addError("name", "Field can't be empty");
+		}
+			
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if (exception.getErros().size() > 0) {
+			throw exception;
+		}
+		
 		obj.setName(txtName.getText());
 		
 		return obj;
@@ -114,5 +135,15 @@ public class DepartmentFormController implements Initializable {
 		txtId.setText (String.valueOf (entity.getId()));
 		txtName.setText(entity.getName());
 		
+	}
+	
+	private void setErrorMessages (Map<String, String> erros) {
+		Set<String> fields = erros.keySet();
+	
+	if (fields.contains("name")) {
+		labelErrorName.setText(erros.get("name"));
+		
+		}
+	
 	}
 }
